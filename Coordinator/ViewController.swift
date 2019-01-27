e4733631c35ece035e09a1664b562d31df4d639e
swiftcode <mpc@junct.com>
2019-01-27 13:26:57 -0600

    Initial Commit

---
+++ Coordinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coordinator
+//
+//  Created by mpc on 1/27/19.
+//  Copyright © 2019 mpc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
